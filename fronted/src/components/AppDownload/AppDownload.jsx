5f9056558278a0580b45a1c45cea89bd255cf5fd
wlaurentec <152199005+wlaurentec@users.noreply.github.com>
2024-10-21 18:54:18 -0500

    add AppDownload component

---
+++ fronted/src/components/AppDownload/AppDownload.jsx
@@ -0,0 +1,16 @@
+import { assets } from "../../assets/assets"
+import "./AppDownload.css"
+
+const AppDownload = () => {
+  return (
+    <div className="app-download" id="app-download">
+      <p>For Better Experience Download <br/> Tomato App</p>
+      <div className="app-download-platforms">
+        <img src={assets.play_store} alt="" />
+        <img src={assets.app_store} alt="" />
+      </div>
+    </div>
+  )
+}
+
+export default AppDownload
