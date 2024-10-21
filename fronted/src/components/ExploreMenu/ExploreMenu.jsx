63d7af4b66d090db46d75a14194e05267b060fd0
wlaurentec <152199005+wlaurentec@users.noreply.github.com>
2024-10-21 11:03:34 -0500

    add ExploreMenu component

--- fronted/src/components/ExploreMenu/ExploreMenu.jsx
+++ fronted/src/components/ExploreMenu/ExploreMenu.jsx
@@ -1,21 +1,27 @@
-import "./ExploreMenu.css"
-import { menu_list } from "../../assets/assets"
-const ExploreMenu = () => {
+import "./ExploreMenu.css";
+import { menu_list } from "../../assets/assets";
+const ExploreMenu = ({category, setCategory}) => {
+  console.log(category)
   return (
     <div className="explore-menu" id="explore-menu">
       <h1>Explore our menu</h1>
-      <p className="explore-menu-text">Choose from our wide range of food items and order them online from your home or office. We deliver to your doorstep. Easy to order and pay online at any time. Choose from our wide range of food items and order them online from your home or office.</p>
-      <div className="explore-menu-list"></div>
+      <p className="explore-menu-text">
+        Choose from our wide range of food items and order them online from your
+        home or office. We deliver to your doorstep.
+      </p>
+      <div className="explore-menu-list">
         {menu_list.map((item, index) => {
           return (
-            <div className="explore-menu-list-item" key={index}>
-              <img src={item.menu_image} alt="" />
+            <div onClick={() => setCategory(prev=>prev === item.menu_name ? "All" : item.menu_name)} className="explore-menu-list-item" key={index}>
+              <img className={category === item.menu_name ? "active" : ""} src={item.menu_image} alt="" />
               <p>{item.menu_name}</p>
             </div>
-          )
+          );
         })}
+      </div>
+      <hr />
     </div>
-  )
-}
+  );
+};
 
-export default ExploreMenu
+export default ExploreMenu;
