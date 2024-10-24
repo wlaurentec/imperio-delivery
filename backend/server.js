e23e202d41c13ba36c7ddf314fd183ef814e5ce9
wlaurentec <152199005+wlaurentec@users.noreply.github.com>
2024-10-24 14:26:22 -0500

    add addFood controller

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 import express from "express";
 import cors from "cors";
 import { connectDB } from "./config/db.js";
+import foodRouter from "./routes/foodRoute.js";
 
 
 
@@ -14,6 +15,10 @@ app.use(cors());
 
 // db connection
 connectDB();
+
+// api endpoints
+app.use("/api/food", foodRouter);
+app.use("/images", express.static("uploads"));
 
 app.get("/", (req, res) => {
   res.send("API Working");
