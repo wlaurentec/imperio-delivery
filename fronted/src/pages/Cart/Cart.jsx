3548a3fb30ed44aa88adefbfc6e1ba6b30c2ad6b
wlaurentec <152199005+wlaurentec@users.noreply.github.com>
2024-10-24 11:20:46 -0500

    add PlaceOrder component

--- fronted/src/pages/Cart/Cart.jsx
+++ fronted/src/pages/Cart/Cart.jsx
@@ -1,9 +1,12 @@
 import { useContext } from "react";
 import "./Cart.css";
 import { StoreContext } from "../../context/StoreContext";
+import { useNavigate } from "react-router-dom";
 
 const Cart = () => {
   const { cartItems, food_list, removeFromCart, getTotalCartAmount } = useContext(StoreContext);
+
+  const navigate = useNavigate();
 
   return (
     <div className="cart">
@@ -49,22 +52,22 @@ const Cart = () => {
             <hr />
             <div className="cart-total-details">
               <p>Delivery Fee</p>
-              <p>${2}</p>
+              <p>${getTotalCartAmount() === 0 ? 0 : 2}</p>
             </div>
             <hr />
             <div className="cart-total-details">
               <b>Total</b>
-              <b>${getTotalCartAmount() + 2}</b>
+              <b>${getTotalCartAmount() === 0 ? 0 : getTotalCartAmount() + 2}</b>
             </div>
           </div>
-          <button>PROCCED TO CHECKOUT</button>
+          <button onClick={() => navigate("/order")}>PROCCED TO CHECKOUT</button>
         </div>
         <div className="cart-promocode">
           <div>
             <p>If you have a promo code, Enter it here</p>
             <div className="cart-promocode-input">
               <input type="text" placeholder="Promo code" />
-              <button>Submit</button>
+              <button >Submit</button>
             </div>
           </div>
         </div>
