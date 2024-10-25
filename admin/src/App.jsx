4596512d23ae52a73f5ef2f4eee2482764351cc8
wlaurentec <152199005+wlaurentec@users.noreply.github.com>
2024-10-25 12:20:41 -0500

    add admin panel strcuture

---
+++ admin/src/App.jsx
@@ -0,0 +1,25 @@
+import Navbar from "./components/Navbar/Navbar";
+import Sidebar from "./components/Sidebar/Sidebar";
+import { Routes, Route } from "react-router-dom";
+import Orders from "./pages/Orders/Orders";
+import List from "./pages/List/List";
+import Add from "./pages/Add/Add";
+
+const App = () => {
+  return (
+    <div>
+      <Navbar />
+      <hr />
+      <div className="app-content">
+        <Sidebar />
+        <Routes>
+          <Route path="/add" element={<Add />} />
+          <Route path="/list" element={<List />} />
+          <Route path="/orders" element={<Orders />} />
+        </Routes>
+      </div>
+    </div>
+  );
+};
+
+export default App;
---
+++ admin/src/pages/Orders/Orders.jsx
@@ -0,0 +1,11 @@
+import "./Orders.css"
+
+const Orders = () => {
+  return (
+    <div>
+      Orders
+    </div>
+  )
+}
+
+export default Orders
