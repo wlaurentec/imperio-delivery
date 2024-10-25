1b86daa0fa2f70ec47b4d2cf89181b464e9faa74
wlaurentec <152199005+wlaurentec@users.noreply.github.com>
2024-10-25 15:19:48 -0500

    add Add component

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -4,10 +4,13 @@ import { Routes, Route } from "react-router-dom";
 import Orders from "./pages/Orders/Orders";
 import List from "./pages/List/List";
 import Add from "./pages/Add/Add";
+import { ToastContainer  } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 const App = () => {
   return (
     <div>
+      <ToastContainer />
       <Navbar />
       <hr />
       <div className="app-content">
--- admin/src/pages/Add/Add.jsx
+++ admin/src/pages/Add/Add.jsx
@@ -1,29 +1,83 @@
 import "./Add.css";
 import { assets } from "../../assets/assets";
+import { useEffect, useState } from "react";
+import axios from "axios";
+import { toast } from "react-toastify";
 
-const Add = () => {
+const Add = () => { 
+  const url = "http://localhost:4000";
+  const [image, setImage] = useState(false);
+  const [data, setData] = useState({
+    name:"",
+    description:"",
+    price:"",
+    category:"Salad"
+  });
+
+  const onChangeHandler = (e) => {
+    const name = e.target.name;
+    const value = e.target.value;
+    setData({ ...data, [name]: value });
+  };
+
+  const onSubmitHandler = async(e) => {
+    e.preventDefault();
+    const formData = new FormData();
+    formData.append("name", data.name);
+    formData.append("description", data.description);
+    formData.append("price", data.price);
+    formData.append("category", data.category);
+    formData.append("image", image);
+    
+    const response = await axios.post(`${url}/api/food/add`, formData);
+    if(response.data.success) {
+      setData({
+        name:"",
+        description:"",
+        price:"",
+        category:"Salad"
+      })
+      setImage(false)
+      toast.success(response.data.message)
+    }
+    else {
+      toast.error(response.data.message)
+    }
+  };
+
+  useEffect(() => {
+    console.log(data);
+  }, [data]);
+
   return (
     <div className="add">
-      <form className="flex-col">
+      <form className="flex-col" onSubmit={onSubmitHandler}>
         <div className="add-img-upload flex-col">
           <p>Upload Image</p>
           <label htmlFor="image">
-            <img src={assets.upload_area} alt="" />
+            <img src={image? URL.createObjectURL(image) :assets.upload_area} alt="" />
           </label>
-          <input type="file" id="image" hidden required />
+          <input onChange={(e) => setImage(e.target.files[0])} type="file" id="image" hidden required />
         </div>
         <div className="add-product-name flex-col">
           <p>Product Name</p>
-          <input type="text" name="name" placeholder="Type here" />
+          <input onChange={onChangeHandler} value={data.name} type="text" name="name" placeholder="Type here" required />
         </div>
         <div className="add-product-description flex-col">
           <p>Product Description</p>
-          <textarea name="description" rows="6" placeholder="Write content here" required></textarea>
+          <textarea
+            onChange={onChangeHandler}
+            value={data.description}
+            name="description"
+            rows="6"
+            placeholder="Write content here"
+            required
+          ></textarea>
         </div>
         <div className="add-category-price">
           <div className="add-category flex-col">
             <p>Product Category</p>
-            <select name="category" >
+            <select onChange={onChangeHandler} name="category">
               <option value="Salad">Salad</option>
               <option value="Rolls">Rolls</option>
               <option value="Desserts">Desserts</option>
@@ -36,10 +90,12 @@ const Add = () => {
           </div>
           <div className="add-price flex-col">
             <p>Prodcut price</p>
-            <input type="Number" name="price" placeholder="$20"/>
+            <input onChange={onChangeHandler} value={data.price} type="Number" name="price" placeholder="$20" />
           </div>
         </div>
-        <button type="submit" className="add-btn">ADD</button>
+        <button type="submit" className="add-btn">
+          ADD
+        </button>
       </form>
     </div>
   );
