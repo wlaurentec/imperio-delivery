c9a78cbb14739cca2574362e7bb116bd3433749a
wlaurentec <152199005+wlaurentec@users.noreply.github.com>
2024-10-26 20:46:30 -0500

    integrate fronted with carController.js

--- fronted/src/context/StoreContext.jsx
+++ fronted/src/context/StoreContext.jsx
@@ -1,5 +1,5 @@
 import { createContext, useEffect, useState } from "react";
-import { food_list } from "../assets/assets";
+import axios from "axios";
 
 export const StoreContext = createContext(null);
 
@@ -7,16 +7,24 @@ const StoreContextProvider = (props) => {
   const [cartItems, setCartItems] = useState({});
   const url = "http://localhost:4000";
   const [token, setToken] = useState("");
-  const addToCart = (itemId) => {
+  const [food_list, setFoodList] = useState([]);
+
+  const addToCart = async (itemId) => {
     if (!cartItems[itemId]) {
       setCartItems((prev) => ({ ...prev, [itemId]: 1 }));
     } else {
       setCartItems((prev) => ({ ...prev, [itemId]: prev[itemId] + 1 }));
     }
+    if(token) {
+      await axios.post(url + "/api/cart/add", { itemId }, { headers: { token } });
+    }
   };
 
-  const removeFromCart = (itemId) => {
+  const removeFromCart = async (itemId) => {
     setCartItems((prev) => ({ ...prev, [itemId]: prev[itemId] - 1 }));
+    if(token) {
+      await axios.post(url + "/api/cart/remove", { itemId }, { headers: { token } });
+    }
   };
 
   const getTotalCartAmount = () => {
@@ -30,10 +38,29 @@ const StoreContextProvider = (props) => {
     return totalAmount;
   };
 
+  const fetchFoodList = async () => {
+    const response = await axios.get(url + "/api/food/list");
+    setFoodList(response.data.data);
+  };
+
+const loadCartData = async (token) => {
+  const response = await axios.post(url + "/api/cart/get", {}, { headers: { token } });
+  setCartItems(response.data.cartData);
+
+}
+
   useEffect(() => {
-    if (localStorage.getItem("token")) {
-      setToken(localStorage.getItem("token"));
+   
+    async function loadData() {
+      await fetchFoodList();
+      if (localStorage.getItem("token")) {
+        setToken(localStorage.getItem("token"));
+        await loadCartData(localStorage.getItem("token"));
+
+      }
     }
+    loadData();
+
   }, []);
 
   const contextValue = {
